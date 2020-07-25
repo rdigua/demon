389adeba77321a30a0a25ff586247c3fb2c59923
rdigua <digua@163.com>
2020-07-26 00:16:53 +0800

    debug:check db

---
+++ src/sqlite.sql
@@ -0,0 +1,20 @@
+begin
+CREATE TABLE day_dealing if not exists day_dealing(
+    number: INTEGER,
+	number:INTEGER,
+	date:INTEGER,
+	open:INTEGER,
+	high:INTEGER,
+	low:INTEGER,
+	close:INTEGER,
+	amount:INTEGER,
+	vol:INTEGER,
+	reservation:INTEGER,
+	constraint KeyName primary key (number,date),
+
+)
+end;
+
+BEGIN
+                CREATE TABLE day_dealing if not exists day_dealing (number integer,date integer,open integer,high integer,low integer,close integer,amount integer,vol integer,reservation integer, constraint keyname primary key (number,date));
+END;
